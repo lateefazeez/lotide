97c740e14131697328066473fc6a6289d726d24e
Lateef Azeez <codelate18@gmail.com>
2021-09-15 02:53:53 +0000

    Refactored correctly

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -8,7 +8,7 @@
 // assertEqual("Lighthouse Labs" , "Bootcamp");
 // assertEqual(1, 1);
 
-const eqArrays = (actual, expected) => {
+const assertArraysEqual = (actual, expected) => {
   let result;
   if (actual.length === expected.length) {
     for (let i = 0; i < actual.length; i ++) {
@@ -24,8 +24,8 @@ const eqArrays = (actual, expected) => {
   }
 };
 
-eqArrays([1, 2, 3], [1, 2, 3]); // => true
-eqArrays([1, 2, 3], [3, 2, 1]); // => false
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
+assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
 
-eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => false
