ad750f965292d28cb296965e9b3527481f667c47
Lateef Azeez <codelate18@gmail.com>
2021-09-15 02:48:02 +0000

    Implemented the asserArraysEqual function

---
+++ assertArraysEqual.js
@@ -0,0 +1,31 @@
+// const assertEqual = function(actual, expected) {
+//   if (actual === expected) {
+//     console.log(`❤️❤️❤️ Assertion Passed: [actual] === ${expected}`);
+//   } else {
+//     console.log(`✅✅✅ Assertion Failed: [actual] !== ${expected}`);
+//   }
+// };
+// assertEqual("Lighthouse Labs" , "Bootcamp");
+// assertEqual(1, 1);
+
+const eqArrays = (actual, expected) => {
+  let result;
+  if (actual.length === expected.length) {
+    for (let i = 0; i < actual.length; i ++) {
+      for (let j = 0; j < expected.length; j++) {
+        if (actual[i] === expected[j]) {
+          result = `❤️❤️❤️ Assertion Passed: [actual] === ${expected}`;
+        } else {
+          result = `✅✅✅ Assertion Failed: [actual] !== ${expected}`;
+        }
+      }
+    }
+    console.log(result);
+  }
+};
+
+eqArrays([1, 2, 3], [1, 2, 3]); // => true
+eqArrays([1, 2, 3], [3, 2, 1]); // => false
+
+eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
