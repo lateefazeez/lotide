76884bd95d65c84cc43c03aa618cce5c2ef9faf4
Lateef Azeez <codelate18@gmail.com>
2021-09-18 21:26:42 +0000

    Fixed the returnomg false bug

--- eqArrays.js
+++ eqArrays.js
@@ -9,26 +9,26 @@ const assertEqual = function(actual, expected) {
 // assertEqual(1, 1);
 
 const eqArrays = (firstArray, secondArray) => {
-  if (firstArray.length === secondArray.length) {
-    for (let i = 0; i < firstArray.length; i++) {
-      firstArray.sort(), secondArray.sort(); //Added sort method
-      if (firstArray[i] !== secondArray[i]) {
-        return false;
-      }
-      
+  if (firstArray.length !== secondArray.length) return false;
+  
+  for (let i = 0; i < firstArray.length; i++) {
+    firstArray, secondArray; //Added sort method
+    if (firstArray[i] !== secondArray[i]) {
+      return false;
     }
   }
+  
   return true;
 };
 
-eqArrays([1, 2, 3], [1, 2, 3]); // => true
-eqArrays([1, 2, 3], [3, 2, 1]); // => false
+// eqArrays([1, 2, 3], [1, 2, 3]); // => true
+// eqArrays([1, 2, 3], [3, 2, 1]); // => false
 
-eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
+// eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
+// eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => should PASS
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => should PASS
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); // => should PASS
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => should PASS
 
