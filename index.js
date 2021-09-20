40f43d96fdb5565889893cdc638548634c30a1cc
Lateef Azeez <codelate18@gmail.com>
2021-09-20 23:07:28 +0000

    cleaned up the index.js objects using the ES6 syntax

---
+++ index.js
@@ -0,0 +1,27 @@
+const head = require("./head");
+const middle = require("./middle");
+const tail = require("./tail");
+const countLetters = require("./countLetters");
+const countOnly = require("./countOnly");
+const eqObjects = require("./eqObjects");
+const eqArrays = require("./eqArrays");
+const findKey = require("./findKey");
+const findKeyByValue = require("./findKeyByValue");
+const flatten = require("./flatten");
+const letterPositions = require("./letterPositions");
+const map = require("./map");
+
+module.exports = {
+  head,
+  tail,
+  middle,
+  countLetters,
+  countOnly,
+  eqObjects,
+  eqArrays,
+  findKey,
+  findKeyByValue,
+  flatten,
+  letterPositions,
+  map
+};
